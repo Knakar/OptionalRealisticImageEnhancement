eb55a86bf0a56120d362902ea892ae18dcc1e60e
Knakar <109785964+Knakar@users.noreply.github.com>
2024-06-13 00:19:29 +0900

    refactor

--- dataloader/anydataset.py
+++ dataloader/anydataset.py
@@ -44,27 +44,3 @@ class AnyDataset:
         
         return {'rgb': rgb, 'mask': mask, 'category':category, 'rgb_org':rgb_org, 'mask_org':mask_org}
         
-
-class ResultDataset:
-    def __init__(self, args, inputs: Image, mask: Image):
-        self.args = args
-        self.rgbs =  inputs
-        self.mask = mask
-        opt = {"load_size": args.load_size, "crop_size": args.crop_size, "preprocess": "resize", "no_flip": True}
-        self.rgb_transform = get_transform(opt, grayscale=False)
-        self.mask_transform = get_transform(opt, grayscale=True)
-        self.org_transform = transforms.Compose([transforms.ToTensor()])
-
-    def __len__(self):
-        return len(self.rgbs)
-
-    def __getitem__(self, index_):
-        rgb_img = Image.fromarray(self.rgbs[index_], "RGB")
-        mask_img = self.mask
-        mask_img = mask_img.resize(tuple(rgb_img.size), Image.Resampling.NEAREST)
-        rgb = self.rgb_transform(rgb_img)
-        mask = self.mask_transform(mask_img)
-        rgb_org = self.org_transform(rgb_img)
-        mask_org = self.org_transform(mask_img)
-        category = torch.Tensor([-1])
-        return {"rgb": rgb, "mask": mask, "category": category, "rgb_org": rgb_org, "mask_org": mask_org}
